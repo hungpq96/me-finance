345f21fa1e31a15fa5c9c375fa714a2de5484afc
hungpq-1271 <pham.quang.hung@sun-asterisk.com>
2020-06-01 10:56:19 +0700

    Config redux

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,19 @@ import "normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import { FocusStyleManager } from "@blueprintjs/core";
+import { Provider } from "react-redux";
 
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import store from "store";
+import App from "App";
+import * as serviceWorker from "serviceWorker";
 
 FocusStyleManager.onlyShowFocusOnTabs();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/store/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+  // manually add reducer here!
+});
+
+export default rootReducer;
